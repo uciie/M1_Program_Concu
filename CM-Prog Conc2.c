76059a87191679185722cdbff2c0cd4d4ea2675b
LUCIE PAN <e45004162@miage-210-003.dmiage.parisnanterre.fr>
2025-09-19 17:14:40 +0200

    fin cours 2

--- CM-Prog Conc2.c
+++ CM-Prog Conc2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#define N 10
 
 void *affCar(void *arg){
     char c;
@@ -10,9 +11,27 @@ void *affCar(void *arg){
     }
 }
 
-int main(void){
+void *affCar2(void *arg){
+    char c;int i;
+    c = * (char *)arg;
+    for (i=0; i<N; i++){
+        putchar( c );
+    }
+}
+
+pthread_t cree_tache(void * (*start_routine)(void *), void * arg){
+    pthread_t id;
+    int erreur;
+    erreur = pthread_create( &id, NULL, start_routine, arg);
+    if (erreur != 0){
+    perror( "Echec creation de tâche Posix" );
+    exit(-1);
+    }
+    return id;
+}
+
+int main1(void){
     char *leCar;
-    
     pthread_t tache_Posix_B;
     pthread_t tache_Posix_C;
 
@@ -25,5 +44,105 @@ int main(void){
     while(1){
         putchar('Z');
     }
+
     return 0;
+}
+
+int main2(void){
+    // Problème : les deux tâches affichent le même caractère, la memoire est partagée
+    // pour regler le problème il faut un sleep entre les deux créations de tâches
+    char leCar;
+    
+    pthread_t tache_Posix_B;
+    pthread_t tache_Posix_C;
+    
+    leCar = 'B';
+    pthread_create( &tache_Posix_B, NULL, affCar, (void*) &leCar);
+    
+    // sleep(1); // pour régler le problème
+
+    leCar = 'C';
+    pthread_create( &tache_Posix_C, NULL, affCar, (void*) &leCar);
+    
+    while(1){
+        putchar('Z');
+    }
+    
+    return 0;
+}
+
+int main3(void){
+    // Problème : on gere mal la terminaison des tâches
+    // pour regler le problème il faut utiliser pthread_join
+    
+    char *leCar; int i;
+    pthread_t tache_Posix_B;
+    pthread_t tache_Posix_C;
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'B';
+    tache_Posix_B = cree_tache( affCar2, (void*) leCar);
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'C';
+    tache_Posix_C = cree_tache( affCar2, (void*) leCar);
+    
+    for (i=0; i<N; i++){
+        putchar('Z');
+    }
+    putchar('\n');
+    
+    return 0;
+}
+
+int main4(void){
+    char *leCar; int i;
+    
+    pthread_t tache_Posix_B;
+    pthread_t tache_Posix_C;
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'B';
+    tache_Posix_B = cree_tache( affCar2, (void*) leCar);
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'C';
+    tache_Posix_C = cree_tache( affCar2, (void*) leCar);
+    
+    for (i=0; i<N; i++){
+        putchar('Z');
+    }
+    // Attente de la terminaison des fils avant de terminer le père
+    pthread_join( tache_Posix_B, NULL);
+    pthread_join( tache_Posix_C, NULL);
+
+    putchar('\n');
+    return 0;
+}
+
+int main5(void){
+    // On force l'ordre d'exécution des tâches
+    char *leCar; int i;
+    
+    pthread_t tache_Posix_B;
+    pthread_t tache_Posix_C;
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'B';
+    tache_Posix_B = cree_tache( affCar2, (void*) leCar);
+    pthread_join( tache_Posix_B, NULL);
+    
+    leCar = (char*) malloc(1*sizeof(char)); *leCar = 'C';
+    tache_Posix_C = cree_tache( affCar2, (void*) leCar);
+    pthread_join( tache_Posix_C, NULL);
+
+    for (i=0; i<N; i++){
+        putchar('Z');
+    }
+
+    putchar('\n');
+    return 0;
+}
+
+int main(void){
+    // return main1();
+    // return main2();
+    // return main3();
+    // return main4();
+     return main5();
 }
--- td_1/td1_N.c
+++ td_1/td1_N.c
@@ -1,97 +1,96 @@
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <math.h>
+#include <sys/wait.h>
+
 #include "td_1_calcul.h"
 
-#define N 50000
-
+#define N 75000
 int K;
 double *tab_max;
 int *tab_x;
 int *tab_y;
 
-int **tube;
+int **tube; // int tube[K][2]
 
-void init(){
-    printf("Entrez le nombre de processus (K>=1) : ");
-    scanf("%d", &K);
-
-    // calloc ou malloc
-    tab_max = (double *)calloc(K, sizeof(double));
-    tab_x   = (int *)calloc(K, sizeof(int));
-    tab_y   = (int *)calloc(K, sizeof(int));
-
-    tube = (int **)calloc(K, sizeof(int *));
-    for (int i = 0; i < K; i++) {
-        tube[i] = calloc(2, sizeof(int));
+void init(int argn, char *argv[])
+{
+    if (argn <2){
+        printf("il faut un parametre : le nombre de processus\n");
+        exit(1);
     }
-}
+    if (sscanf(argv[1], "%d", &K)<1){
+        printf("Il faut un entier\n");
+        exit(1);
+    };
+    tab_max = (double *)calloc(K, sizeof(double));
+    tab_x = (int *)calloc(K, sizeof(int));
+    tab_y = (int *)calloc(K, sizeof(int));
 
-int main(void){
-    init();
-    for (int i = 0; i < K; i++) {
+    tube = calloc(K, sizeof(int *));
+    for (int i = 0; i < K; tube[i++] = (int *)calloc(2, sizeof(int)))
+        ;
+}
+int main(int argn, char *argv[])
+{
+    init(argn, argv);
+    for (int i = 0; i < K; i++)
+    {
         pipe(tube[i]);
         int k = fork();
-        if (k > 0) { // code père
+        if (k > 0)
+        {
             close(tube[i][1]);
         }
-        else { // code fils
-            printf("fils %d créé\n", i);
-            close(tube[i][0]); // ferme lecture
-
-            for (int x = i; x <= N; x += K) {
-                for (int y = 0; y <= N; y++) {
-                    float val = f(x,y);
-                    if (fabs(val) > tab_max[i]) {
-                        tab_max[i] = val;
+        else
+        {
+            //printf("demarrage du fils %d\n", i);
+            close(tube[i][0]);
+            for (int x = i; x <= N; x += K)
+            {
+                for (int y = 0; y <= N; y++)
+                {
+                    float cal = f(x, y);
+                    if (fabs(cal) > tab_max[i])
+                    {
+                        tab_max[i] = cal;
                         tab_x[i] = x;
                         tab_y[i] = y;
                     }
                 }
             }
-
             write(tube[i][1], &tab_max[i], sizeof(tab_max[i]));
             write(tube[i][1], &tab_x[i], sizeof(tab_x[i]));
             write(tube[i][1], &tab_y[i], sizeof(tab_y[i]));
+            //printf("fin du fils %d, max = %.2f pour x=%d\n", i, tab_max[i], tab_x[i]);
             close(tube[i][1]);
-            printf("fin fils %d\n", i);
             exit(0);
         }
     }
 
     double max = 0;
-    int x_max = 0;
-    int y_max = 0;
-
-    for (int p = 0; p < K; p++) {
-        double tmp_max;
-        int tmp_x, tmp_y;
-
-        read(tube[p][0], &tmp_max, sizeof(tmp_max));
-        read(tube[p][0], &tmp_x, sizeof(tmp_x));
-        read(tube[p][0], &tmp_y, sizeof(tmp_y));
-
-        if (tmp_max > max) {
-            max = tmp_max;
-            x_max = tmp_x;
-            y_max = tmp_y;
+    int x, y;
+    for (int i = 0; i < K; i++)
+    {
+        read(tube[i][0], &max, sizeof(max));
+        read(tube[i][0], &x, sizeof(x));
+        read(tube[i][0], &y, sizeof(y));
+    }
+    for (int i = 0; i < K; i++)
+    {
+        wait(NULL);
+        close(tube[i][0]);
+    }
+    //printf("calcul du max\n");
+    for (int i = 0; i < K; i++){
+        if (tab_max[i]> max){
+            max = tab_max[i];
+            x = tab_x[i];
+            y = tab_y[i];
         }
     }
-
-    for (int p = 0; p < K; p++) {
-        wait(NULL);
-    }
-    printf("Le max est atteint en (x=%d, y=%d) et vaut %f\n", x_max, y_max, max);
-
-    free(tab_max);
-    free(tab_x);
-    free(tab_y);
-    for (int i = 0; i < K; i++) {
-        free(tube[i]);
-    }
-    free(tube);
+    printf("le max est atteint en (x=%d, y=%d) et vaut %.2f\n", x, y, max);
 }
